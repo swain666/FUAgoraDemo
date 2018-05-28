6550a5c1d066db5371758671be1c1b71383e5778
jdbbjd <1090336995@qq.com>
2018-05-28 14:19:34 +0800

    upload OC Demo

---
+++ demo/FUOpenLive-OC/OpenLive/KeyCenter.m
@@ -0,0 +1,15 @@
+//
+//  KeyCenter.m
+//  OpenLive
+//
+//  Created by GongYuhua on 2016/9/12.
+//  Copyright © 2016年 Agora. All rights reserved.
+//
+
+#import "KeyCenter.h"
+
+@implementation KeyCenter
++ (NSString *)AppId {
+    return @"b646785207b84110be190b025b1f4b3c";
+}
+@end
